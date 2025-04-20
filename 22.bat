6011f7dea63f74d6a99f88e725c1ac047faccfbf
MRNF <mrnfqrbl@gmail.com>
2025-04-21 07:33:16 +0800

    2025-04-21-html和插件更改

---
+++ 22.bat
@@ -0,0 +1,21 @@
+@echo on
+chcp 65001
+set PYTHONUNBUFFERED=1
+set PYTHONUTF8=1
+cd /d D:\参考\ComfyUI
+
+echo -= 检查是否需要安装依赖 =-
+if "%1"=="-i" (
+  echo 接收到 -i 参数，执行安装依赖步骤
+  echo -= Install custom nodes dependencies =-
+  D:\参考\ComfyUI\.venv\Scripts\pip.exe install GitPython
+
+  D:\参考\ComfyUI\.venv\Scripts\python.exe D:\参考\ComfyUI\custom_nodes\ComfyUI-Manager/cm-cli.py restore-dependencies
+) else (
+  echo 未接收到 -i 参数，跳过安装依赖步骤
+)
+
+echo -= 启动 ComfyUI =-
+D:\参考\ComfyUI\.venv\Scripts\python.exe D:\参考\ComfyUI\main.py --directml --lowvram
+
+cmd /k
