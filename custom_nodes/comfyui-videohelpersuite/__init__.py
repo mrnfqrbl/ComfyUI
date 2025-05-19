62b443d1da87552e2875b17fb0d647a3734554a6
MRNF <mrnfqrbl@gmail.com>
2025-05-19 21:14:47 +0800

    Remove submodules from index

--- custom_nodes/comfyui-videohelpersuite/__init__.py
+++
@@ -1,9 +0,0 @@
-from .videohelpersuite.nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-import folder_paths
-from .videohelpersuite.server import server
-from .videohelpersuite import documentation
-from .videohelpersuite import latent_preview
-
-WEB_DIRECTORY = "./web"
-__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
-documentation.format_descriptions(NODE_CLASS_MAPPINGS)
--- custom_nodes/comfyui-videohelpersuite/tests/README.md
+++
@@ -1,1 +0,0 @@
-Workflows for automated testing of VHS. Most include an additional tests key to check the properties or perform comparisons on node outputs
